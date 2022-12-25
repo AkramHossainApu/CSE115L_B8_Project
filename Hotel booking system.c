6f842f9541fbdbc113a793c20a965f06dcff4b00
AkramHossainOpu <akramhossainopu2002@gmail.com>
2022-12-25 15:05:44 +0600

    Created timed welcome massage

--- Hotel booking system.c
+++ Hotel booking system.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+#include <unistd.h>
 
+void printWelcome();
+
+int main() {
+    printWelcome();
+
+    return 0;
+}
+
+void printWelcome() {
+    printf("\t    ----------------------------------------------------------------------------\n");
+	printf("\t   |                                                                            |\n");
+	printf("\t   |  WW              WW EEEEEEE LL      CCCCCCC  OOOOOO  MM        MM EEEEEEE  |\n");
+	printf("\t   |   WW            WW  EE      LL      CC      OO    OO MM MM  MM MM EE       |\n");
+	printf("\t   |    WW    WW    WW   EEEEE   LL      CC      OO    OO MM   MM   MM EEEEE    |\n");
+	printf("\t   |     WW WW  WW WW    EE      LL      CC      OO    OO MM        MM EE       |\n");
+	printf("\t   |      WW      WW     EEEEEEE LLLLLLL CCCCCCC  OOOOOO  MM        MM EEEEEEE  |\n");
+	printf("\t   |                                                                            |\n");
+	printf("\t    ----------------------------------------------------------------------------\n");
+	sleep(2);
+    system("cls");
+}
